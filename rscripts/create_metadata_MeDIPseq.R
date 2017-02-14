fc648377b61f7072609afee126ff8b1a11f8d46f
welch16 <welch16@gmail.com>
2017-02-14 14:55:25 -0600

    base of the script to generate design_file for MeDIPseq corr matrix computation

---
+++ rscripts/create_metadata_MeDIPseq.R
@@ -0,0 +1,32 @@
+
+rm(list = ls())
+
+library(magrittr)
+library(dplyr)
+library(readr)
+
+indir = "data/BAM/hg19/bowtie_dip"
+files = list.files(indir,full.names = TRUE,pattern = "sort.bam")
+files = files[grep("bai",files,invert = TRUE)]
+
+
+lab = ifelse(grepl("Nok",basename(files)),"Scott","Rona")
+
+cell = ifelse(lab == "Rona",
+              ifelse(grepl("akata",basename(files)),"EBV","NOK"),
+              ifelse(grepl("EBV",basename(files)),"EBV","NOK"))
+
+treat = ifelse(lab == "Rona",
+               ifelse(grepl("no_treatment",basename(files)),"None",
+                      ifelse(grepl("methyl",basename(files)),"MC","CaFBS")),
+               ifelse(grepl("mono",basename(files)),"None","MC"))
+
+repl = basename(files) %>% strsplit("rep") %>% unlist
+repl = repl[grepl("genes.results",repl)]
+repl = paste0("Rep", gsub(".genes.results","",repl) )
+
+df  = tibble( file = files,
+              cell , treatment = treat,lab , rep = repl)
+
+write_delim(df , path = "data/metadata/PCA_definition.tsv" ,
+            delim = "\t")
