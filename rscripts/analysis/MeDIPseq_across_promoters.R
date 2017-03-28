0ad709c0bfc233371e1bfaf81a718775942f58c5
welch16 <welch@stat.wisc.edu>
2017-03-28 13:23:55 -0500

    added count matrix of dipseq data

---
+++ rscripts/analysis/MeDIPseq_across_promoters.R
@@ -0,0 +1,84 @@
+
+## this script builds a count matrix with the MeDIP-seq data, across a set of genes
+##
+## 1 - gets the ensembl hg19 genes
+## 2 - converts them to promoters
+## 3 - reads the MeDIP-seq files, and count the number of extended reads in
+##     the promoters regions
+
+rm(list = ls())
+
+library(biomaRt)
+library(tidyverse)
+library(GenomicAlignments)
+
+
+## Functions to search for ensembl genes
+#    listMarts()
+#    grch37 = useMart(biomart="ENSEMBL_MART_ENSEMBL", host="grch37.ensembl.org", path="/biomart/martservice")
+#    listDatasets(grch37)
+
+
+## Get gene positions for all ensembl genes in hg19
+ensembl = useMart(biomart = "ENSEMBL_MART_ENSEMBL",dataset = "hsapiens_gene_ensembl")
+attr <- c("chromosome_name", "start_position", "end_position",
+          "ensembl_gene_id")
+bm <- getBM(attr, "chromosome_name", c(as.character(1:22),"X","Y"), ensembl)
+rd <- with(bm,
+           RangedData(IRanges(start_position, end_position),
+                      space=chromosome_name, ensembl_gene_id))
+rd = rd %>% as("GRanges")
+seqlevels(rd) = paste0("chr",seqlevels(rd))
+
+
+## define promoters according to Grimm et al, 2012
+upstream = 500
+downstream = 1000
+fraglen = 300
+
+promoters = promoters(rd, upstream,downstream)
+
+## get MeDIP-seq data, we are going to extend the reads by 300 bps
+## and count the number of reads in the promoter regions
+
+dipdr = "data/BAM/hg19/bowtie_dip"
+dipfiles = list.files(dipdr,pattern = "sort",full.names = TRUE) %>%
+    .[grep("bai",.,invert = TRUE)]
+
+## count overlapping  reads into promoters regions
+process_reads <- function(file,promoters,fraglen)
+{
+    w = width(promoters) %>% max
+    which = resize(promoters, w + 2 * fraglen,fix = "center")
+    reads = readGAlignments(file,param = ScanBamParam(which = which))
+
+    reads = reads %>% as("GRanges") %>% resize(fraglen)
+    countOverlaps(promoters,reads) 
+    
+}    
+
+
+library(BiocParallel)
+
+bp = MulticoreParam(workers = 10,bpprogressbar = TRUE)
+
+countvectors = bplapply(dipfiles,process_reads,promoters,fraglen,BPPARAM = bp)
+names(countvectors) = gsub(".sort.bam","",basename(dipfiles))
+
+outdr = "data/MeDIPseq_results"
+save(countvectors,file = file.path(outdr,"MeDIPseq_counts.RData"))
+
+## tidying them up into wide data
+count_matrix = bind_cols(
+    promoters %>% as.data.frame %>% as.tbl %>%
+      select(ensembl_gene_id,everything()) %>%
+      mutate(width = NULL,strand = NULL),
+    countvectors %>% do.call(cbind,.) %>% as.data.frame %>% as.tbl)
+
+write_tsv(count_matrix,
+          path = file.path(outdr,
+                           paste0("MeDIPseq_PromotersCounts_upstr",upstream,
+                                  "_downstr",downstream,"fraglen",fraglen,".tsv")))
+    
+
+
