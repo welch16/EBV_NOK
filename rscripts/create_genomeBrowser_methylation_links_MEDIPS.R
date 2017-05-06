09e418b4bc99c735f410a8baff2afea5ff8cd22a
Rene Welch <welch16@gmail.com>
2017-05-05 21:11:53 -0500

    changed the name

--- rscripts/create_genomeBrowser_methylation_links_MEDIPS.R
+++ rscripts/create_genomeBrowser_methylation_links_MEDIPS.R
@@ -9,6 +9,7 @@ ucsc_url = "http://genome.ucsc.edu/cgi-bin/hgTracks?hgS_doOtherUser=submit&hgS_o
 context = 1e3
 
 diff_meth_anno = diff_meth_anno %>%
+  filter(methSet %in% c("mC.up","mC.down")) %>%
   mutate(
     edgeR.logCPM = NULL,
     strand = NULL,
@@ -16,13 +17,15 @@ diff_meth_anno = diff_meth_anno %>%
     geneId = NULL
   ) %>% 
   mutate_if(is.logical,funs(ifelse(.,1,0))) %>%
-  mutate(url_pos = paste0(ucsc_url, seqnames,":",start - context ,"-",end + context)) %>%
-  select(url_pos , everything()) %>%
+  mutate(url = paste0(ucsc_url, seqnames,":",start - context ,"-",end + context)) %>%
+  select(url , everything()) %>% 
   select(-contains("rpkm")) %>%
   select(-contains("rms")) %>%
-  rename(chrom = seqnames) %>%
+  rename(chrom = seqnames) %>% 
+  mutate(geneStrand = ifelse(geneStrand == "+","F","R")) %>%
   split(.$methSet) %>%
-  map(mutate,methSet = NULL)
+  map(mutate,methSet = NULL) %>%
+  map( .f = function(x)x %>% arrange(desc(medlog10pval)))
 
 dr = "data/MEDIPS/UCSC_link"
 
@@ -31,14 +34,53 @@ dir.create(dr,showWarnings = FALSE)
 diff_meth_anno %>% 
   map2(
     names(diff_meth_anno) %>% 
-      {gsub(".up","EBV_dominant",.)} %>%
-      {gsub(".down","NOKS_dominant",.)},
+      {gsub(".up","_EBV_dominant",.)} %>%
+      {gsub(".down","_NOKS_dominant",.)},
     .f = function(tab,pref){
       write_tsv(tab,path = file.path(dr,paste0(pref,".tsv")))
     })
   
   
   
+load("data/MEDIPS/DiffMethyl_annotation.RData")
+
+ucsc_url = "http://genome.ucsc.edu/cgi-bin/hgTracks?hgS_doOtherUser=submit&hgS_otherUserName=welch16&hgS_otherUserSessionName=hMC_tracks&position="
+context = 1e3
+
+diff_meth_anno = diff_meth_anno %>%
+  filter(methSet %in% c("hmC.up","hmC.down")) %>%
+  mutate(
+    edgeR.logCPM = NULL,
+    strand = NULL,
+    width = NULL,min.pval = NULL,med.pval = NULL,
+    geneId = NULL
+  ) %>% 
+  mutate_if(is.logical,funs(ifelse(.,1,0))) %>%
+  mutate(url = paste0(ucsc_url, seqnames,":",start - context ,"-",end + context)) %>%
+  select(url , everything()) %>% 
+  select(-contains("rpkm")) %>%
+  select(-contains("rms")) %>%
+  rename(chrom = seqnames) %>% 
+  mutate(geneStrand = ifelse(geneStrand == "+","F","R")) %>%
+  split(.$methSet) %>%
+  map(mutate,methSet = NULL) %>%
+  map( .f = function(x)x %>% arrange(desc(medlog10pval)))
+
+dr = "data/MEDIPS/UCSC_link"
+
+dir.create(dr,showWarnings = FALSE)
+
+diff_meth_anno %>% 
+  map2(
+    names(diff_meth_anno) %>% 
+    {gsub(".up","_EBV_dominant",.)} %>%
+    {gsub(".down","_NOKS_dominant",.)},
+    .f = function(tab,pref){
+      write_tsv(tab,path = file.path(dr,paste0(pref,".tsv")))
+    })
+
+
+
 
 
 
