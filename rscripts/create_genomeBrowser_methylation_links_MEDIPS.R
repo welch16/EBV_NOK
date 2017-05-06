152a3434f040719acc0630529d97351a05a396e8
Rene Welch <welch16@gmail.com>
2017-05-05 20:35:13 -0500

    added script to generate links for diff methylated regions

---
+++ rscripts/create_genomeBrowser_methylation_links_MEDIPS.R
@@ -0,0 +1,45 @@
+
+rm(list = ls())
+
+library(tidyverse)
+
+load("data/MEDIPS/DiffMethyl_annotation.RData")
+
+ucsc_url = "http://genome.ucsc.edu/cgi-bin/hgTracks?hgS_doOtherUser=submit&hgS_otherUserName=welch16&hgS_otherUserSessionName=MC_tracks&position="
+context = 1e3
+
+diff_meth_anno = diff_meth_anno %>%
+  mutate(
+    edgeR.logCPM = NULL,
+    strand = NULL,
+    width = NULL,min.pval = NULL,med.pval = NULL,
+    geneId = NULL
+  ) %>% 
+  mutate_if(is.logical,funs(ifelse(.,1,0))) %>%
+  mutate(url_pos = paste0(ucsc_url, seqnames,":",start - context ,"-",end + context)) %>%
+  select(url_pos , everything()) %>%
+  select(-contains("rpkm")) %>%
+  select(-contains("rms")) %>%
+  rename(chrom = seqnames) %>%
+  split(.$methSet) %>%
+  map(mutate,methSet = NULL)
+
+dr = "data/MEDIPS/UCSC_link"
+
+dir.create(dr,showWarnings = FALSE)
+
+diff_meth_anno %>% 
+  map2(
+    names(diff_meth_anno) %>% 
+      {gsub(".up","EBV_dominant",.)} %>%
+      {gsub(".down","NOKS_dominant",.)},
+    .f = function(tab,pref){
+      write_tsv(tab,path = file.path(dr,paste0(pref,".tsv")))
+    })
+  
+  
+  
+
+
+
+
