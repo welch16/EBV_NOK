2a8fad39b75e2a8f39b7c1f07363a028d2ee1a6b
welch16 <welch@stat.wisc.edu>
2017-05-08 11:35:03 -0500

    updated diff. expression script to use test statistic as signal to noise measure

--- rscripts/perform_differential_expression_contrast_analysis.R
+++ rscripts/perform_differential_expression_contrast_analysis.R
@@ -54,10 +54,7 @@ opt$A_Tr = separateFiles(opt$A_Tr)
 opt$B_Tr = separateFiles(opt$B_Tr)
 
 library(tximport,quietly = TRUE)
-library(readr,quietly = TRUE)
-library(dplyr,quietly = TRUE)
-library(tidyr,quietly = TRUE)
-library(ggplot2,quietly = TRUE)
+library(tidyverse,quietly = TRUE)
 
 source("rfuns/geneExpression_analysis.R")
 
@@ -115,62 +112,25 @@ deseq_model1 = DESeq(deseq,minReplicatesForReplace = Inf)
 
 my_results = list()
 my_results[["full"]] = results(deseq_model1,cooksCutoff = FALSE)
-my_results[["cell"]] = results(deseq_model1,contrast = c("cell",cells),cooksCutoff = FALSE)
+my_results[["cell"]] = results(deseq_model1,contrast = c("cell",rev(cells)),cooksCutoff = FALSE)
 my_results[["treat"]] = results(deseq_model1,contrast = c("treat",rev(treat)),cooksCutoff = FALSE)
 
-get_columns <- function(cond,columns)
-{
-    if(is.null(cond)){
-        out = rep(TRUE,length(columns))
-    }else{
-        if(grepl(",",cond,fixed = TRUE)){
-            cond = cond %>% strsplit(",") %>% unlist
-            out = lapply(cond,function(x)grepl(x,columns))
-            out = do.call(and,out)
-        }else{
-            out = grepl(cond,columns)
-        }
-    }
+## get_columns <- function(cond,columns)
+## {
+##     if(is.null(cond)){
+##         out = rep(TRUE,length(columns))
+##     }else{
+##         if(grepl(",",cond,fixed = TRUE)){
+##             cond = cond %>% strsplit(",") %>% unlist
+##             out = lapply(cond,function(x)grepl(x,columns))
+##             out = do.call(and,out)
+##         }else{
+##             out = grepl(cond,columns)
+##         }
+##     }
 
-    columns[out]
-}
-
-signal_to_noise <- function(results,model,condA,condB,cells,treats)
-{
-    
-    ## Under their model rhe counts for gene i and condition j are distributed
-    ## K_ij ~ NegBin(mu_ij , alpha_i)
-    ## where mu_ij is the mean and alpha_i is the gene specific dispersion
-    ## We define
-    ## signaltonoise_i = log2FC_i / 2* alpha_i
-
-    ## cond1 & cond2 contains the restrictions of the hypothesis test
-    ## ',' means 'and'
-    ## '_' means 'when'
-
-
-    num = results$log2FoldChange
-    
-    ## mu = assays(model)[["mu"]]
-    ## columns = mu %>% colnames
-
-    ## columnsA = get_columns(condA,columns)
-    ## columnsB = get_columns(condB,columns)
-
-    ## muA = rowMeans(mu[,columns %in% columnsA])
-    ## muB = rowMeans(mu[,columns %in% columnsB])
-    
-    alpha = dispersions(model)
-   
-    num / (2 * alpha)
-}
-
-signal2noise = list()
-signal2noise[["full"]] = signal_to_noise(my_results[["full"]],deseq_model1,paste(cells[1],treat[1],sep = ","),
-                                         paste(cells[2],treat[2],sep = ","),cells,treat)
-
-signal2noise[["cell"]] = signal_to_noise(my_results[["cell"]],deseq_model1,cells[1],cells[2],cells,treat)
-signal2noise[["treat"]] = signal_to_noise(my_results[["treat"]],deseq_model1,treat[2],treat[1],cells,treat)
+##     columns[out]
+## }
 
 design(deseq) <- ~ interac
 deseq_model2 = DESeq(deseq, minReplicatesForReplace = Inf)
@@ -195,20 +155,11 @@ my_results[[paste0("cell_",cells[1])]] = results(deseq_model2,
 my_results[[paste0("cell_",cells[2])]] = results(deseq_model2,
     contrast = c("interac",grepv(cells[2],interac[[1]]) %>% rev),cooksCutoff = FALSE)
 
-signal2noise[[paste0("cell_",cells[1])]] = signal_to_noise(my_results[[paste0("cell_",cells[1])]],
-    deseq_model2,paste(cells[1],treat[2],sep = ","),paste(cells[1],treat[1],sep = ","),cells,treat)
-signal2noise[[paste0("cell_",cells[2])]] = signal_to_noise(my_results[[paste0("cell_",cells[2])]],
-    deseq_model2,paste(cells[2],treat[2],sep = ","),paste(cells[2],treat[1],sep = ","),cells,treat)
-
 my_results[[paste0("treat_",treat[1])]] = results(deseq_model2,
     contrast = c("interac",grepv(treat[1],interac[[1]]) %>% rev), cooksCutoff = FALSE)
 my_results[[paste0("treat_",treat[2])]] = results(deseq_model2,
     contrast = c("interac",grepv(treat[2],interac[[1]]) %>% rev), cooksCutoff = FALSE)
 
-signal2noise[[paste0("treat_",treat[1])]] = signal_to_noise(my_results[[paste0("treat_",treat[1])]],
-    deseq_model2,paste(treat[1],cells[1],sep = ","),paste(treat[1],cells[2],sep = ","),cells,treat)
-signal2noise[[paste0("treat_",treat[2])]] = signal_to_noise(my_results[[paste0("treat_",treat[2])]],
-    deseq_model2,paste(treat[2],cells[1],sep = ","),paste(treat[2],cells[2],sep = ","),cells,treat)
 
 models = names(my_results)
 
@@ -216,15 +167,15 @@ genes = my_results[[1]] %>% rownames
 
 #%>% strsplit("_") %>% sapply(function(x)x[2])
 
-clean_results <- function(res,s2n, genes)
+clean_results <- function(res, genes)
 {   
     res %>% as.data.frame %>%
         as.tbl %>% mutate(gene = genes) %>%
-        select(gene,baseMean,log2FC = log2FoldChange,pvalue,padj) %>%
-        mutate(signal2noise = s2n)
+        select(gene,everything())
 }
 
-my_results_DF = mapply(clean_results,my_results,signal2noise,MoreArgs = list(genes),SIMPLIFY = FALSE)
+my_results_DF = my_results %>% map(clean_results,genes)
+
 
 rename_cols <- function(x,label)
 {
@@ -234,8 +185,8 @@ rename_cols <- function(x,label)
     x    
 }
 
-my_results_DF = mapply(rename_cols,my_results_DF,names(my_results_DF),
-                       SIMPLIFY = FALSE)
+my_results_DF = my_results_DF %>% map2(names(my_results_DF),rename_cols)
+
 
 my_results = Reduce(function(...) merge(..., by='gene', all.x=TRUE),my_results_DF) %>% as.tbl
 
@@ -248,14 +199,15 @@ pvalue_histogram <- function(mod,my_results)
     dd = my_results %>% select(gene,contains(mod))
 
     if(mod %in% c("cell","treat")){
-        dd = dd[,1:6]
+        dd = dd[,1:7]
     }
 
     dd = dd %>%
         select(gene,contains("pvalue"))
     names(dd) = c("gene","pvalue")
 
-    dd %>% ggplot(aes(pvalue)) + geom_histogram(bins = 150) +
+    dd %>% ggplot(aes(pvalue)) +
+        geom_histogram(bins = 50,colour = "blue",fill = "white",boundary = 0) +
         ggtitle(mod)
 
 }
@@ -268,23 +220,24 @@ dev.off()
 
 volcano_plot <- function(mod,my_results,fdr = opt$fdr)
 {
-    
-    nms = c("gene","log2FC","pvalue","padj")
+    nms = c("gene","log2FoldChange","pvalue","padj")
     dd = my_results %>% select(gene,contains(mod))
 
     if( mod %in% c("cell","treat")){
-        dd = dd[,1:6]
+        dd = dd[,1:7]
     }
 
     dd = dd %>%
-        select(gene,contains("FC"),contains("pval"),contains("padj") )
+        select(gene,contains("Fold"),contains("pval"),contains("padj") )
     names(dd) = nms
+
+    
 
     dd = dd %>% mutate(col = ifelse(padj <= fdr,"yes","no")) %>% filter(!is.na(padj))
 
-    mm = 1.3 * min(min(dd$log2FC),max(dd$log2FC)) %>% abs
+    mm = 1.3 * min(min(dd$log2FoldChange),max(dd$log2FoldChange)) %>% abs
 
-    dd %>% ggplot(aes(log2FC,-log10(pvalue),colour = col))+geom_point()+
+    dd %>% ggplot(aes(log2FoldChange,-log10(pvalue),colour = col))+geom_point()+
         scale_colour_brewer(name = paste("adjusted pval <=",fdr),palette = "Set1")+
         theme(legend.position = "top")+ggtitle(mod)+xlim(-mm,mm)+
         geom_vline(xintercept = 0,linetype = 2)
@@ -299,20 +252,20 @@ dev.off()
 
 signal2noise_barplot <- function(mod,my_results)
 {    
-  
+
     dd = my_results %>% select(gene,contains(mod))
 
     if(mod %in% c("cell","treat")){
-        dd = dd[,1:6]
+        dd = dd[,1:7]
     }
 
     names(dd) = gsub(paste0(mod,":"),"",names(dd) )
 
-    dd = dd %>% arrange(signal2noise) %>%
+    dd = dd %>% arrange(stat) %>%
         mutate(gene = factor(gene,levels = gene),
                rank = seq_len(nrow(dd)))
 
-    dd %>% ggplot(aes(rank,signal2noise))+geom_line()+
+    dd %>% ggplot(aes(rank,stat))+geom_line()+
         ggtitle(mod)+geom_abline(slope = 0,intercept = 0,linetype = 2)
 }
 
